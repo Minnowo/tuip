2dcbb44aaaae294fb47f36a66c91877d461eac7e
Minnowo <62451415+Minnowo@users.noreply.github.com>
2024-05-15 00:22:47 -0400

    simplify cdt

--- scripts/cdt.sh
+++ scripts/cdt.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 
+# source this file in your .bashrc, or copy the function below
+
 # Pick a directory and cd into it
 # useful if you cannot type the name of the directory
 function cdt() {
 
-    files=()
-    files+=(..) # always include parent dir first
-
     # makes */ not appear in the arrays
     shopt -s nullglob
 
-    # hidden directories
-    for i in .*/; do
-
-        files+=("$i")
-    done
-
-    # other directories
-    for i in */; do
-
-        files+=("$i")
-    done
+    files=()
+    files+=(..)  # always include parent dir first
+    files+=(.*/) # hidden dirs
+    files+=(*/)  # other dirs
 
     shopt -u nullglob
 
